1300b84ed491b896dca1217fd423a57a5a99f3f5
AnasSM0 <134851867+AnasSM0@users.noreply.github.com>
2025-01-28 16:27:52 +0500

    Random Float and int with Heads and Tails

--- random Number generator.py
+++ random Number generator.py
@@ -3,4 +3,13 @@ print("Welcome to random number generator.")
 import random
 
 random_integer = random.randint(1,100)
-print (random_integer)
+if random_integer % 2 == 0:
+    print("Heads")
+
+else: 
+    print("Tails")
+ 
+
+
+# random_Float = random.random() * 100
+# print(random_Float)
