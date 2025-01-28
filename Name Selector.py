8f9e5fb3b2b714c948130f80bc3b7f908c546539
AnasSM0 <134851867+AnasSM0@users.noreply.github.com>
2025-01-28 16:47:03 +0500

    RandInt and RandChoices

---
+++ Name Selector.py
@@ -0,0 +1,11 @@
+import random
+
+friends = ["Alice","Bob","Charles","Dixie","Elon","Felicia"]
+
+# random_integer= random.randint(0,5)
+
+# randomName = friends[random_integer]
+
+# print(randomName)
+
+print(random.choices(friends))
