4eaba57ef94e78abeec9f81971fc7dc8d441ced4
AnasSM0 <134851867+AnasSM0@users.noreply.github.com>
2025-01-30 23:53:53 +0500

    Random Password generator

---
+++ Highest Score.py
@@ -0,0 +1,18 @@
+Score = [130,184,33,199,163,155,41,103,155,72,200,172,3,28,135,77,191,65,142,186]
+
+total_Score = sum(Score)
+
+Sum=0
+
+for s in Score:
+    Sum+=s
+
+print(s)
+
+MaxScore = max(Score)
+print (MaxScore)
+
+Max_Score=0
+for s in Score:
+    if s >= Max_Score:
+        Max_Score = s
