c385480b46f3c1857375659e6fab655b4e41a7b3
AnasSM0 <134851867+AnasSM0@users.noreply.github.com>
2025-04-27 21:55:36 +0500

    dictionary and nested dictionary

---
+++ dictionaries.py
@@ -0,0 +1,15 @@
+capitals = {
+    "France" : "Paris",
+    "Germany" : "Berlin"
+}
+
+travel_log = {
+    "France" : ["Paris", "Lille", "Dijon"],
+    "Germany" : ["Stuttgart", "Berlin"]
+}
+
+print (travel_log["France"][1])
+
+nested_list=["A","B",["C","D"]]
+
+print(nested_list[])
