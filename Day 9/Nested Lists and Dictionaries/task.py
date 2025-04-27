2a3c31b20e7477c981a4466007567213a6a90bc0
AnasSM0 <134851867+AnasSM0@users.noreply.github.com>
2025-04-27 18:42:23 +0500

    first commit

---
+++ Day 9/Nested Lists and Dictionaries/task.py
@@ -0,0 +1,12 @@
+capitals = {
+    "France" : "Paris",
+    "Germany" : "Berlin"
+}
+
+travel_log = {
+    "France" : ["Paris", "Lille", "Dijon"],
+    "Germany" : ["Stuttgart", "Berlin"]
+}
+
+print (travel_log["France"][1])
+
