4b38ead2f18b4672bfa2d3dd591ccf19f1b9dacc
AnasSM0 <134851867+AnasSM0@users.noreply.github.com>
2025-08-24 16:21:34 +0500

    blackjack

--- blackjack/main.py
+++ blackjack/main.py
@@ -29,8 +29,13 @@ comp_score = calc_score(comp_cards)
 print(f"Your cards: {user_cards}, Current Score: {user_score}")
 print(f"Computer's first card: {comp_cards[0]}") 
 
-#if user_score == 0 or comp_score == 0 or user_score > 21:
-#  is_game_over = True
+if user_score == 0 or comp_score == 0 or user_score > 21:
+  is_game_over = True
 
-
+else: 
+    should_deal = input("Type 'y' to get another card or 'n' to pass:")
+    if should_deal == "y":
+        user_cards.append(deal())
+    else: 
+        is_game_over = True
 
