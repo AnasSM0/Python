3bdfe36cf89bf14d7187524484e4cf1301672748
AnasSM0 <134851867+AnasSM0@users.noreply.github.com>
2025-08-28 15:40:30 +0500

    number guessing game

---
+++ number guessing game/main.py
@@ -0,0 +1,27 @@
+from art import logo
+import random 
+
+
+    
+
+
+
+random_number = random.randint(1,100)
+print(logo)
+print("\nWelcome to the number guessing game")
+print("I'm thinking of a number between 1 and 100")
+
+difficulty = input("Select a difficulty. Type 'easy' or 'hard': ")
+
+attempts = 0
+
+if difficulty == "easy":
+    attempts = 10
+elif difficulty == "hard":
+    attempts = 5
+else: 
+    print("Invalid option, Please try again.")
+
+print(f"You have {attempts} attempts left to guess.")
+user_guess = input("Make a guess: ")
+
