460ce08d4e61dcace138e1e90a6b523494b704d8
AnasSM0 <134851867+AnasSM0@users.noreply.github.com>
2025-10-14 10:21:55 +0500

    coffee machine fixed

--- coffee machine/main.py
+++ coffee machine/main.py
@@ -30,7 +30,43 @@ resources = {
     "coffee": 100,
 }
 
+quarters = 0 #0.25
+dimes = 0 #0.10
+nickles = 0 #0.05
+pennies = 0 #0.01
+
 choice = input("“What would you like? (espresso/latte/cappuccino): ")
 
 if choice == "espresso":
-    
+    if resources["water"] >=MENU["espresso"]["ingredients"]["water"] and resources["coffee"] >= MENU["espresso"]["ingredients"]["coffee"]:
+        quarters = int(input("How many quarters: "))* 0.25
+        dimes = int(input("How many dimes: "))* 0.10
+        nickles = int(input("How many nickles: "))* 0.05
+        pennies = int(input("How many pennies: "))* 0.01
+        total = quarters + dimes + nickles + pennies
+        if total >= MENU["espresso"]["cost"]:
+            change = round(total - MENU["espresso"]["cost"],2)
+            print(f"Here is ${change} in change.")
+            print("Here is your espresso ☕️. Enjoy!")
+            resources["water"] -= MENU["espresso"]["ingredients"]["water"]
+            resources["coffee"] -= MENU["espresso"]["ingredients"]["coffee"]
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+
+elif choice == "cappuccino":
+    if resources["water"] >=MENU["cappuccino"]["ingredients"]["water"] and resources["coffee"] >= MENU["cappuccino"]["ingredients"]["coffee"]:
+        quarters = int(input("How many quarters: "))* 0.25
+        dimes = int(input("How many dimes: "))* 0.10
+        nickles = int(input("How many nickles: "))* 0.05
+        pennies = int(input("How many pennies: "))* 0.01
+        total = quarters + dimes + nickles + pennies
+        if total >= MENU["cappuccino"]["cost"]:
+            change = round(total - MENU["cappuccino"]["cost"],2)
+            print(f"Here is ${change} in change.")
+            print("Here is your cappuccino ☕️. Enjoy!")
+            resources["water"] -= MENU["cappuccino"]["ingredients"]["water"]
+            resources["coffee"] -= MENU["cappuccino"]["ingredients"]["coffee"]
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+
+        
