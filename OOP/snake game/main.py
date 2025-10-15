92290bcdf9cf525e800c038f6cbcfb8d9ced8372
AnasSM0 <134851867+AnasSM0@users.noreply.github.com>
2025-10-15 13:22:31 +0500

    snake game using turtle graphics

---
+++ OOP/snake game/main.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, Screen
+from random import randint
+
+screen = Screen()
+screen.setup(width=600,height=600)
+screen.title("Snake Game")
+screen.bgcolor("black")
+
+tom = Turtle()
+tom.shape("square")
+tom.color("white")
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
