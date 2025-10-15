a35ffa171594c3ce2d2732646f0d633c8e859329
AnasSM0 <134851867+AnasSM0@users.noreply.github.com>
2025-10-15 10:46:39 +0500

    classes in python

---
+++ OOP/working with classes/main.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+
+user1 = User(1, "Alice")
+user2 = User(2, "Bob")
+print(user1.id, user1.name)
+print(user2.id, user2.name)
