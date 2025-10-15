03e9355914a540a3e23d0c621b0fb637ddd96377
AnasSM0 <134851867+AnasSM0@users.noreply.github.com>
2025-10-15 10:48:05 +0500

    classes in python

--- OOP/working with classes/main.py
+++ OOP/working with classes/main.py
@@ -2,9 +2,12 @@ class User:
     def __init__(self, id, name):
         self.id = id
         self.name = name
+        self.followers = 0 
+        
 
 
 user1 = User(1, "Alice")
 user2 = User(2, "Bob")
 print(user1.id, user1.name)
 print(user2.id, user2.name)
+print(user1.followers)
